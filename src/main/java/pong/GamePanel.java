f021fb98a1d3da4ca3ac6035587068d305b66284
henrikbuller <henbul0928@goteborg.skola.se>
2021-02-01 13:17:24 +0100

    Added GamePanel.java

---
+++ src/main/java/pong/GamePanel.java
@@ -0,0 +1,51 @@
+package hb_pong;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+	public GamePanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void newBall() {
+
+	}
+
+	public void newPaddles() {
+
+	}
+
+	public void paint(Graphics g) {
+
+	}
+
+	public void draw(Graphics g) {
+
+	}
+
+	public void move() {
+
+	}
+
+	public void checkCollision() {
+
+	}
+
+	public void run() {
+
+	}
+
+	public class AL extends KeyAdapter {
+		public void KeyPressed(KeyEvent e) {
+
+		}
+
+		public void KeyReleased(KeyEvent e) {
+
+		}
+	}
+}
