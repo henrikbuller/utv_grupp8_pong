b07f75ec222e2271f3a6dd034329682000664b0a
henrikbuller <henbul0928@goteborg.skola.se>
2021-02-02 14:11:53 +0100

    configured test path and GamePanelTest

---
+++ src/test/java/pong/GamePanelTest.java
@@ -0,0 +1,14 @@
+package pong;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GamePanelTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
